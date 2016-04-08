08a2fd5b23caee60946fc30fcd43a78bbeb89a12
Abhimanyu Babbar <abhimanyu.babbar@wrapp.com>
2016-04-08 22:44:01 +0200

    Added the base version of tokenizer for crawling the webpage.

--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"strconv"
 	"time"
+
+	"golang.org/x/net/html"
 )
 
 // http://www.gocomics.com/calvinandhobbes/2016/04/06
@@ -44,28 +45,138 @@ func main() {
 	fmt.Println("Started looking for your comics.")
 	now := time.Now()
 
+	done := make(chan bool)
 	for _, comic := range comics {
-
-		dFormat := dateFormat(comic, now)
-		switch comic {
-		case "calvin":
-			resp, err := http.Get(CALVIN + dFormat)
-			if err != nil {
-				panic(err)
-			}
-			bytes, _ := ioutil.ReadAll(resp.Body)
-			fmt.Println(string(bytes))
-		case "dilbert":
-			resp, err := http.Get(DILBERT + dFormat)
-			if err != nil {
-				panic(err)
-			}
-			bytes, _ := ioutil.ReadAll(resp.Body)
-			fmt.Println(string(bytes))
-
-		}
+		go fetch(comic, now, done)
 	}
 
+	fmt.Println("Synchronizing the responses.")
+
+	// Wait for as many responses as the comics.
+	for range comics {
+		<-done
+	}
+}
+
+func fetch(comic string, time time.Time, done chan bool) {
+
+	dFormat := dateFormat(comic, time)
+
+	defer func() {
+		done <- true
+	}()
+
+	switch comic {
+	case "calvin":
+		resp, err := http.Get(CALVIN + dFormat)
+		if err != nil {
+			return
+		}
+
+		parentFound := false
+
+		defer resp.Body.Close()
+		z := html.NewTokenizer(resp.Body)
+
+		for {
+			tokenType := z.Next()
+
+			switch tokenType {
+			case html.ErrorToken:
+				return
+
+			case html.StartTagToken:
+				token := z.Token()
+				if token.Data == "div" {
+
+					for _, attr := range token.Attr {
+						if attr.Key == "class" &&
+							attr.Val == "feature" {
+							// found the div for the comic.
+							fmt.Println("Found the parent container div > Calvin")
+							parentFound = true
+						}
+					}
+				}
+
+			case html.SelfClosingTagToken:
+
+				token := z.Token()
+				// Locate the image under the parent div
+				if token.Data == "img" && parentFound {
+					for _, attr := range token.Attr {
+
+						// attributes key and values.
+						if attr.Key == "alt" &&
+							attr.Val == "Calvin and Hobbes" {
+
+							fmt.Println("Found the img token > Calvin")
+							// again iterate to check for the
+							// values
+							for _, attributes := range token.Attr {
+								if attributes.Key == "src" {
+									fmt.Println(attributes.Val)
+									break
+								}
+							}
+						}
+					}
+				}
+
+			}
+		}
+	case "dilbert":
+		resp, err := http.Get(DILBERT + dFormat)
+		if err != nil {
+			return
+		}
+		parentFound := false
+		defer resp.Body.Close()
+
+		html.NewTokenizer(resp.Body)
+		z := html.NewTokenizer(resp.Body)
+
+		for {
+			tokenType := z.Next()
+
+			switch tokenType {
+			case html.ErrorToken:
+				return
+
+			case html.StartTagToken:
+				token := z.Token()
+				if token.Data == "div" {
+
+					for _, attr := range token.Attr {
+						if attr.Key == "class" &&
+							attr.Val == "img-comic-container" {
+							// found the div for the comic.
+							fmt.Println("Found the parent container div > Dilbert")
+							parentFound = true
+						}
+					}
+				}
+
+			case html.SelfClosingTagToken:
+
+				token := z.Token()
+				// Locate the image under the parent div
+				if token.Data == "img" && parentFound {
+
+					fmt.Println("Found the image tag > Dilbert")
+					for _, attr := range token.Attr {
+						if attr.Key == "src" {
+							fmt.Println(attr.Val)
+							break
+						}
+					}
+				}
+
+			}
+		}
+	default:
+		fmt.Println("Not a valid comic for downloading: " + comic)
+	}
 }
 
 func dateFormat(comic string, time time.Time) string {
@@ -88,4 +199,8 @@ func dateFormat(comic string, time time.Time) string {
 	}
 
 	return format
+}
+
+func crawl(url string) error {
+	return nil
 }
